4f7f83e9b03e44cd57917899f6810ed57a5a0126
dornelasxz <rafaeldornelasalmeida@gmail.com>
2023-08-17 09:28:37 -0300

    primeiroo commit

---
+++ aula2.C
@@ -0,0 +1,44 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+int main(){
+    
+    float nota1, nota2, nota3, media;
+    char turno;
+    
+    printf ("Digite o turno\n");
+    scanf("%c", &turno);
+
+    printf("Digite notas:\n");
+    scanf("%f" , &nota1);
+    scanf("%f" , &nota2);
+    scanf("%f" , &nota3);
+    
+    media = (nota1 + nota2 + nota3)/3;
+    if(media >=7 && media <=8){
+        printf("Bom!\n");
+    }
+    else if(media >= 9){
+        printf("Exelente!!!\n");
+    }
+    
+    
+    printf("MEDIA: %f.\n" , media);
+    
+        
+    if(turno == 'm'){
+        printf("Aluno do turno matutino.\n");
+    }
+    else if(turno == 'n'){
+        printf("Aluno do turno noturno.\n");
+    }
+    else {
+        printf("O turno digitado e invalido!!\n");
+    }
+    if(media >= 7){
+        printf("Aluno aprovado.\n");
+    } else {
+        printf("Aluno reprovado.\n");
+    }
+    return 0;
+}
