5c293c8d8e7b2b10302ee660aa4dad216fe4a5f2
dornelasxz <rafaeldornelasalmeida@gmail.com>
2023-08-17 11:18:08 -0300

    primeira calc

---
+++ calculadora.c
@@ -0,0 +1,18 @@
+#include <stdio.h> 
+
+    int main(){
+        int numero1;
+        int numero2;
+        int resultado;
+
+        numero1 = 20622662;
+        numero2 = 10;
+        resultado = numero1 % numero2;
+
+        
+
+
+
+        printf("ultimo algarismo de %d: %d\n", numero1, resultado);
+        return 0;
+    }
